66d95f94b797c99e4c873e7ad30bb78767f7779b
Latcho <latcho.nikolov@gmail.com>
2018-11-20 17:38:25 +0200

    search is responsive and gives purples too

--- mainscript.js
+++ mainscript.js
@@ -141,6 +141,7 @@ function loadSearch(searchText){
 	var err = 0;
 	var gdd = 0;
 	var unkwn = 0;
+	var prpl = 0;
 	var list = document.getElementById('machineRow');
 	var errList = document.createElement('div');
 	errList.setAttribute('id', 'errorList');
@@ -148,6 +149,8 @@ function loadSearch(searchText){
 	okList.setAttribute('id', 'okList');
 	var unknownList = document.createElement('div');
 	unknownList.setAttribute('id', 'unknownList');
+	var purpleList = document.createElement('div');
+	purpleList.setAttribute('id', 'purpleList');
 	function itterate(varriable){
 		varriable.forEach(function(element) {
 		console.log(element.machineNumber + " " + element.name);
@@ -169,7 +172,7 @@ function loadSearch(searchText){
 			divElement.classList.add('col-1');
 			divElement.appendChild(imageButton);
 			divElement.appendChild(imageText);
-			if (status != 'good' && status != 'unknown'){
+			if (status != 'good' && status != 'unknown' && status != 'purple'){
 				imageButton.src = 'button_round_red.png';
 				errList.appendChild(divElement);
 				err += 1;
@@ -187,9 +190,15 @@ function loadSearch(searchText){
 				unkwn += 1;
 				count += 1;
 			}
+			if (status == 'purple'){
+				imageButton.src = 'button_round_purple.png';
+				purpleList.appendChild(divElement);
+				prpl += 1;
+				count += 1;
+			}
 		
 		});
-		var inHtml = errList.innerHTML + okList.innerHTML + unknownList.innerHTML;
+		var inHtml = errList.innerHTML + purpleList.innerHTML + okList.innerHTML + unknownList.innerHTML;
 		list.innerHTML = inHtml;
 		var unknownsP = document.getElementById('error');
 		unknownsP.innerHTML = 'Машини в грешка: ' + err;
