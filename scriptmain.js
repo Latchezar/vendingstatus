10523f0ae7f2e7dcad00dd675f69b0c6c6cb963e
Latcho <latcho.nikolov@gmail.com>
2018-11-24 06:57:09 +0200

    fixed getMachineByID:

--- scriptmain.js
+++ scriptmain.js
@@ -165,7 +165,7 @@ function searchClick(){
 function getMachineById(id){
 	var url = "http://46.10.241.187:4040/api/machines/" + id;
 	var result;
-	$get(url, function(data){
+	$.get(url, function(data){
 		result = data;
 	});
 	return result;
